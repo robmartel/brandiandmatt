6dee53c989efa53ac92778b40a4e19b618a144ee
robmartel <99683835+robmartel@users.noreply.github.com>
2022-11-21 17:30:01 -0500

    added function to registry page and styling

--- src/Components/registry/Registry.js
+++ src/Components/registry/Registry.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Container, Icon } from './RegistryElements';
+import { Container, Icon, RegistryPic, RegistryH1, ImgWrapper, Img, RegistryP, ImageLink, RegistryBtn, RegistryBtnLink } from './RegistryElements';
 import { GiRose } from 'react-icons/gi';
-
 
 const Registry = () => {
   return (
@@ -10,6 +9,21 @@ const Registry = () => {
    <Icon to='/'>
             <GiRose /> 
             </Icon>
+            <RegistryPic src={require('../../images/brandimatt.jpg')} alt='two people'>
+
+            </RegistryPic>
+            <RegistryH1>Registry</RegistryH1>
+            <ImgWrapper>
+            <ImageLink to="https://www.amazon.ca/wedding/brandi-martel-mathieu-jacques-sault-ste-marie-july-2023/registry/3KP05CJKWV1KU" target="-blank" rel="external">
+            <Img src={require('../../images/amazon.png')} alt='image' />
+            </ImageLink>
+            <RegistryP>Click <ImageLink to="https://www.amazon.ca/wedding/brandi-martel-mathieu-jacques-sault-ste-marie-july-2023/registry/3KP05CJKWV1KU" target="-blank" rel="external">here</ImageLink>, or
+ on the image for Brandi and Matt's Registry Page</RegistryP>
+            </ImgWrapper>
+            <RegistryBtn>
+              <RegistryBtnLink to="/rsvp">RSVP</RegistryBtnLink>
+            </RegistryBtn>
+
     </Container>
   )
 }
--- src/Components/registry/RegistryElements.js
+++ src/Components/registry/RegistryElements.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
 min-height: 692px;
-position: fixed;
+position: absolute;
 bottom: 0;
 left: 0;
 right: 0;
 top: 0;
 z-index: 0;
-overflow: hidden;
+overflow: scroll;
 background: linear-gradient(108deg, rgba(3, 165, 252, 1) 0%, rgba(230, 230, 250) 100%);
 `
 
@@ -26,3 +26,80 @@ font-size: 32px;
     margin-top: 8px;
 }
 `
+export const RegistryPic = styled.img`
+border-radius: 50%;
+height: 120px;
+width: 120px;
+display: flex;
+position: relative;
+float: right;
+align-items: center;
+top: 32px;
+right: 75px;
+
+@media screen and (max-width: 820px) {
+    margin-left: 16px;
+    margin-bottom: 60px;
+    margin-top: 0;
+    height: 75px;
+    width: 75px;
+}
+`
+
+export const RegistryH1 = styled.h1`
+text-align: center;
+font-weight: 700;
+color: white;
+`
+
+export const ImgWrapper = styled.div`
+max-width: 555px;
+height: auto;
+margin: 24px auto;
+`
+
+export const Img = styled.img`
+width: 100%;
+padding-right: 0;
+`
+export const ImageLink = styled(Link)`
+text-decoration: none;
+color: black;
+
+&:hover {
+    color: #fff;
+}
+`
+
+export const RegistryP = styled.p`
+text-align: center;
+margin-top: 12px;
+font-weight: 600;
+`
+
+export const RegistryBtn = styled.nav`
+display: flex;
+justify-content: center;
+align-items: center;
+margin-right: 24px;
+`
+
+export const RegistryBtnLink = styled(Link)`
+border-radius: 50px;
+background: #fff;
+white-space: nowrap;
+padding: 10px 22px;
+color: black;
+font-size: 16px;
+border: none;
+outline: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+
+&:hover{
+    transition: all 0.2s ease-in-out;
+    background: rgb(207, 3, 252);
+    color: #fff;
+}
+`;
