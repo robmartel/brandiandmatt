9f0e2ca3f14d5f930b15589a784f545e9cb92911
robmartel <99683835+robmartel@users.noreply.github.com>
2022-11-21 10:44:15 -0500

    changed registry component and added registry route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 // import NavBar from './Components/NavBar';
 import Home from './Pages/Home';
+import RegistryPage from './Pages/RegistryPage';
 // import CeremonyPage from './Pages/CeremonyPage';
 import RSVPPage from './Pages/RSVPPage';
 
@@ -16,6 +17,10 @@ function App() {
           <Route
           path='/rsvp'
           element={<RSVPPage />}
+        />
+        <Route
+        path='/registry'
+        element={<RegistryPage />}
         />
       </Routes>
       </div>
--- src/Components/registry/Registry.js
+++ src/Components/registry/Registry.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormInput, FormButton, Text } from './RsvpElements'
 
 const Registry = () => {
   return (
     <>
-      <Container>
-        <FormWrap>
-            <Icon to='/'>logo</Icon>
-            <FormContent>
-                <Form action="#">
-                    <FormH1>RSVP to Brandi and Mathieu's Wedding</FormH1>
-                    <FormLabel htmlFor='for'>Email</FormLabel>
-                    <FormInput type='email' required />
-                    <FormLabel htmlFor='for'>Password</FormLabel>
-                    <FormInput type='password' required />
-                    <FormButton type='submit'>Submit</FormButton>
-                    <Text>Forgot password</Text>
-                </Form>
-            </FormContent>
-        </FormWrap>
-      </Container>
+   <h1>Click Here for the registry</h1>
     </>
   )
 }
